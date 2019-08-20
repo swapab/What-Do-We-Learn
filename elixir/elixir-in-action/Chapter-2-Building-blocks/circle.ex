318aa9c872ccc8c8fd6959e7166fab33ad7aebe5
Swapnil Abnave <imswapab@gmail.com>
2019-08-20 18:28:23 +0200

    [elixir-in-action] type-specs using @spec

--- elixir/elixir-in-action/Chapter-2-Building-blocks/circle.ex
+++ elixir/elixir-in-action/Chapter-2-Building-blocks/circle.ex
@@ -7,10 +7,12 @@ defmodule Circle do
   @doc """
   Computes the area of a circle
   """
+  @spec area(number) :: number
   def area(r), do: r*r*@api
 
   @doc """
   Computes the circumference of a circle
   """
+  @spec circumference(number) :: number
   def circumference(r), do: 2*r*@pi
 end
